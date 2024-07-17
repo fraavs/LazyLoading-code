6e69b1a31f0e774a1035cfc3c8c7ed0096af99c9
fraavs <ftcinfraavs@gmail.com>
2024-07-18 07:17:42 +0900

    created Lazy Loading of Module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'first', loadChildren: () =>
+      import('./first/first.module').then(m => m.FirstModule)
+  },
+  {
+    path: 'second', loadChildren: () =>
+      import('./second/second.module').then(m => m.SecondModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
