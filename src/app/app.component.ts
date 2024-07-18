650f2dd1db54c9543a86b95eae801847b79a769a
fraavs <ftcinfraavs@gmail.com>
2024-07-18 09:35:03 +0900

    created unsubscribed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,22 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  observable = new Observable((observer) => {
-    observer.next(1);
-    observer.next(2);
-    observer.next(3);
-    setTimeout(() => {
-      console.log('---After two seconds---');
-      observer.next(4);
-      observer.complete();
-    }, 2000);
-  });
+  observable = new Observable<number>((observer) => {
+    let count = 0;
 
+    setInterval(() => {
+      observer.next(count++);
+    }, 1000);
+
+  });
   constructor() {
-    console.log('Before subscribe');
-    this.observable.subscribe({
-      next(x){
-        console.log('Value:', x);
-      },
-      error(e){
-        console.log('Error occurred: ', e);
-      },
-      complete(){
-        console.log('Observable sucessfully executed');
-      }
+    const obs = this.observable.subscribe((data) => {
+      console.log("Data: ", data);
     });
-    console.log('After subscribe');
+
+    setTimeout(() => {
+      obs.unsubscribe();
+      console.log('Unsubscribed');
+    }, 5000);
   }
 }
