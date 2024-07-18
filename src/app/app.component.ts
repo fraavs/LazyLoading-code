28c189ffefe68e1e8fd930951a9dabb7b805c783
fraavs <ftcinfraavs@gmail.com>
2024-07-18 09:27:58 +0900

    created new observables.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,29 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   observable = new Observable((observer) => {
-    observer.next('Hello');
-    observer.error('An error occurred');
-    observer.complete();
-  }).subscribe({
-    next(value) {
-      console.log('Received value : ', value);
-    },
-    error(err) {
-      console.log('Error : ', err);
-    },
-    complete() {
-      console.log('Observable completed ...')
-    }
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    setTimeout(() => {
+      console.log('---After two seconds---');
+      observer.next(4);
+      observer.complete();
+    }, 2000);
   });
+
+  constructor() {
+    console.log('Before subscribe');
+    this.observable.subscribe({
+      next(x){
+        console.log('Value:', x);
+      },
+      error(e){
+        console.log('Error occurred: ', e);
+      },
+      complete(){
+        console.log('Observable sucessfully executed');
+      }
+    });
+    console.log('After subscribe');
+  }
 }
