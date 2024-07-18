a559f81e1e2e35373d1b8c63fd9c15c34e603427
fraavs <ftcinfraavs@gmail.com>
2024-07-18 09:11:59 +0900

    created observable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LazyLoading-code';
+  observable = new Observable((observer) => {
+    observer.next('Hello');
+  }).subscribe({
+    next(value) {
+      console.log('Received value : ', value);
+    },
+  });
 }
